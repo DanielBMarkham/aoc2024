ee60e235c8bbac20ecfe3ab1ba6315a82146d757
Daniel Markham <danielbmarkham@hotmail.com>
2024-12-15 13:19:18 -0500

    out of time for a few days

--- day2/old-scratch-template.fsx
+++ day2/old-scratch-template.fsx
@@ -19,6 +19,62 @@ let isSafe (a:bool,b:bool,c:bool):bool = (a && (b || c))
 
 // Lesson was to break up any kind of branching logic into separate pieces and test
 
-
+let foo a b = a*b
 // PART 2
+
+// There are two new situations
+// 1. Ascending/Descending not okay, but removing first mismatch fixes
+// 2. Gap too big but removing first one also fixes
+
+let allMismatches list1 list2 =
+  let temp = List.zip list1 list2 
+  List.fold (fun acc  x->
+    if fst x <> snd x then List.append ([fst x]) acc else acc
+    ) [] temp
+let allMatches list1 list2 =
+  let temp = List.zip list1 list2 
+  List.fold (fun acc  x->
+    if fst x = snd x then List.append ([fst x]) acc else acc
+    ) [] temp
+let countMatches list1 list2 =
+  let temp = List.zip list1 list2 
+  let ret =
+    List.fold (fun acc  x->
+      if fst x = snd x 
+        then (List.append ([fst x]) (fst acc), (snd acc)+1)
+        else acc
+      ) ([],0) temp
+  snd ret 
+
+let getUntilMatchFailsN n list1 list2 =
+  let temp = List.zip list1 list2 
+  let ret =
+    List.fold (fun acc  x->
+      if fst x <> snd x 
+        then 
+          if snd acc >0 
+            then
+              (List.append ([fst x]) (fst acc), (snd acc)-1)
+            else acc
+        else acc
+      ) ([],n) temp
+  fst ret 
+  
+let getUntilMatchFailsX n (list1:int list) (list2:int list):int list =
+  let temp = List.zip list1 list2 
+  let ret =
+    List.fold (fun acc  (x:int*int)->
+      if snd acc > 0
+        then 
+          if fst x <> snd x 
+          then
+            let newList=List.append [(fst x)] (fst acc)
+            (newList,snd acc-1) 
+          else 
+            (fst acc,snd acc)
+        else
+            let newList=List.append [(fst x)] (fst acc)
+            (newList,snd acc-1) 
+        ) (List.empty,n) temp
+  fst ret 
 
